3f2699636de97b0dacd690c38af79591d4f3f78d
Egor Rudkov <gren236@gmail.com>
2020-04-23 20:55:36 +0300

    Add basic context

---
+++ go_by_example/context/context.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	// Context is available on every request
+	ctx := r.Context()
+	fmt.Println("Hello handler started")
+	defer fmt.Println("Hello handler ended")
+
+	// Simulate workload using sleep
+	// Cancel working if Done channel received command
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Fprintln(w, "Hello!")
+	case <-ctx.Done():
+		err := ctx.Err()
+		fmt.Println("server:", err)
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":9000", nil)
+}
