aaa19dc70c1f91872dc07dc262cb03f19b886e5b
gren236 <gren236@gmail.com>
2020-05-11 22:21:03 +0300

    Add context example + fix README

---
+++ the_go_blog/context/context.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
+type myKey int
+
+const key myKey = 0
+
+func main() {
+	// Create the context that can be canceled
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Use waitgroup for orchestration
+	var wg sync.WaitGroup
+	wg.Add(10)
+
+	// Create ten goroutines the will derive a Context from the one created above.
+	for i := 0; i < 10; i++ {
+		go func(id int) {
+			defer wg.Done()
+
+			// Derive a new context for this goroutine from the main func's Context.
+			ctx := context.WithValue(ctx, key, id)
+
+			// Wait until the context is cancelled.
+			<-ctx.Done()
+			fmt.Println("Canceled:", id)
+		}(i)
+	}
+
+	// Cancel the context and any derived contexts as well.
+	cancel()
+	wg.Wait()
+}
